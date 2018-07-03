9c157a526e3f6dc10eae154755b16d1da20e3cf3
lishijie <sparkingarthur@163.com>
2018-07-03 19:05:41 +0800

    1.delete try_catch statements since they make no sense in a web server and it is difficult to debug with try_catch statements

--- sqliteauthenticator/sqliteauthenticator.py
+++ sqliteauthenticator/sqliteauthenticator.py
@@ -1,4 +1,4 @@
-#coding=utf-8
+# coding=utf-8
 from jupyterhub.auth import Authenticator
 from tornado import gen
 
@@ -9,6 +9,7 @@ from binascii import b2a_hex, a2b_hex
 
 import sqlite3
 
+
 class prpcrypt():
     def __init__(self, key='jupyterhubkeykey'):
         self.key = key
@@ -18,50 +19,45 @@ class prpcrypt():
     def encrypt(self, text):
         cryptor = AES.new(self.key, self.mode, self.key)
         count = len(text)
-        #padding to length
+        # padding to length
         add = self.length - (count % self.length)
         text = text + ('\0' * add)
         self.ciphertext = cryptor.encrypt(text)
-        #turn to hex string
+        # turn to hex string
         return b2a_hex(self.ciphertext)
-     
+
 
 class SQLiteAuthenticator(Authenticator):
-    def _verify_password(self,username,password):
+    def _verify_password(self, username, password):
         encryptor = prpcrypt()
-        try:
-            sql_cnn = sqlite3.connect(os.getenv('JUPYTERHUB_SQLITEDB_PATH'))
-            # print("connect db sucessfully")
-            cursor = sql_cnn.cursor()
-            sql = ("SELECT password FROM users WHERE username = '{}'").format(username)  # select from the database
-            # print(sql)
-            cursor.execute(sql)
-            user_password = cursor.fetchone()[0] # first to check the username
-            #print(user_password)
-            input_password = encryptor.encrypt(password).decode()
-            if user_password == input_password:
-                cursor.close()
-                sql_cnn.close()
-                return True
-            else:
-                cursor.close()
-                sql_cnn.close()
-                return False
-        except:
-            try:
-                cursor.close()
-                sql_cnn.close()
-            except:
-                pass
-            finally:
-                print("wrong database/username/password, "
-                      "please check your JUPYTERHUB_SQLITEDB_PATH/username/password....")
-                return False
+        sql_cnn = sqlite3.connect(os.getenv('JUPYTERHUB_SQLITEDB_PATH'))
+        print("connect sqlitedb sucessfully")
+        cursor = sql_cnn.cursor()
+        sql = ("SELECT `password` FROM `users` WHERE `username` = '{}'").format(username)  # select from the database
+        # print(sql)
+        cursor.execute(sql)
+        user_password = cursor.fetchone()[0]  # first to check the username
+        # print(user_password)
+        input_password = encryptor.encrypt(password).decode()
+        # print(input_password)
+        # print(user_password == input_password)
+        if user_password == input_password:
+            cursor.close()
+            sql_cnn.close()
+            return True
+        else:
+            print("wrong database/username/password, "
+                  "please check your JUPYTERHUB_SQLITEDB_PATH/username/password....")
+            cursor.close()
+            sql_cnn.close()
+            return False
+
     @gen.coroutine
     def authenticate(self, handler, data):
         username = data['username']
         passwd = data['password']
-        if self._verify_password(username,passwd):
+        # print(passwd)
+        if self._verify_password(username, passwd):
             return data['username']
         else:
             return None
