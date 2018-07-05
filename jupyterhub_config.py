d31547b138b821bb0ad80c75325eb409bb10ad77
lishijie <sparkingarthur@163.com>
2018-07-05 16:39:58 +0800

    1. update: adduser_cmd in jupyterhub_config.py

--- jupyterhub_config.py
+++ jupyterhub_config.py
@@ -1,5 +1,5 @@
 c.JupyterHub.bind_url = 'http://:9002'
-c.Authenticator.add_user_cmd =  ['adduser', '--home', '/home/USERNAME']
+c.Authenticator.add_user_cmd =  ['adduser', '--home', '/home/work/USERNAME']
 c.LocalAuthenticator.create_system_users = True
 c.Authenticator.delete_invalid_users = True
 c.JupyterHub.authenticator_class = 'sqliteauthenticator.SQLiteAuthenticator'
