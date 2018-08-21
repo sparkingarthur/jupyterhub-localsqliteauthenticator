ef8f06d01d560aeb7512b0587af5fe3d6c9dc128
sparkingarthur <sparkingarthur@163.com>
2018-08-21 15:56:44 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='sqliteauthenticator',
-      version='0.1',
+      version='0.11',
       description='SQLite Authenticator for Jupyterhub',
       author='sparkingarthur',
       license='MIT',
